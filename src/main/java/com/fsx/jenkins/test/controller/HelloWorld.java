5f23530a42bcef026c102a416bc7db911b99b5c9
上侠 <fengshangxia@qq.com>
2025-09-20 14:58:45 +0800

    init

---
+++ src/main/java/com/fsx/jenkins/test/controller/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.fsx.jenkins.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/helloWorld")
+    public String HelloWorld() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String date = now.format(formatter);
+        return "现在时间时:" + date;
+    }
+}
